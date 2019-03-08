f689e1c4a20c68fe70577baa74af4755aefca3bc
rileylyman <rileylyman@berkeley.edu>
2019-03-07 17:58:26 -0800

    Implement core server functionality

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <pthread.h>
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,9 +17,12 @@ static struct int_queue clients_waiting;
 static struct atomic_counter num_clients;
 static int server_id;
 
+static int port;
+
 typedef void command_handler(int);
 
 void initialize_thread_pool(struct int_queue *, size_t);
+void serve(void);
 void *thread_pool_worker(void *);
 void handle_connection(int);
 command_handler *parse_command(char *, size_t);
@@ -35,11 +41,38 @@ int main(int argc, char **argv)
 
     initialize_thread_pool(&clients_waiting, THREAD_POOL_SIZE);
 
-    int i;
-    for (i = 0; i < 10; i++)
+    serve();
+}
+
+void serve()
+{
+    int sockfd = socket(PF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in server_addr, client_addr;
+    size_t client_addr_len = sizeof(client_addr);
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+
+    bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    listen(sockfd, 1024);
+
+    while (1)
     {
-        queue_push(&clients_waiting, 1);
+        int client_fd = accept(sockfd, (struct sockaddr *) &client_addr, (socklen_t *) sizeof(client_addr));
+        if (client_fd < 0)
+        {
+            printf("Error accepting connection.\n");
+            continue;
+        }
+        printf("Accepted connection from %s on port %d\n", 
+                inet_ntoa(client_addr.sin_addr), client_addr.sin_port);
+        queue_push(&clients_waiting, client_fd);
     }
+    
+    shutdown(sockfd, SHUT_RDWR);
+    close(sockfd);
 }
 
 void handle_who(int conn_fd)
@@ -73,6 +106,8 @@ void handle_command_not_found(int conn_fd)
  * */
 command_handler *parse_command(char *buffer, size_t len)
 {
+    printf("Got command: %s\n", buffer);
+
     if (! strcmp(buffer, "WHO"))
         return handle_who;
     else if (! strcmp(buffer, "WHERE"))
@@ -89,11 +124,14 @@ void handle_connection(int conn_fd)
     char buffer[READ_BUFFER_SIZE] = {'\0'};
     while ((amt = read(conn_fd, buffer, READ_BUFFER_SIZE) > 0))
     {
+        printf("Received %zu bytes\n", amt);
         buffer[amt] = '\0';
         command_handler *handler = parse_command(buffer, amt);
         handler(conn_fd);
-        printf("Handled connection\n");
+        printf("\nHandled connection\n");
     }
+    printf("Closing connection\n");
+    close(conn_fd);
 }
 
 void initialize_thread_pool(struct int_queue *queue, size_t size)
